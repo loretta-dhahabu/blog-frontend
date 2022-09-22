a46d21a241cc87527058c4e8ddbd625b2aec8fd1
loretta dhahabu <loreydjefwa@gmail.com>
2022-09-22 22:05:49 +0300

    create navbar

---
+++ blog/src/components/Header.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+
+function Header () {
+    return (
+      <Navbar bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="#home">Blog</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#about">About</Nav.Link>
+            <Nav.Link href="#signin">SignIn</Nav.Link>
+          </Nav>
+        </Container>
+        <Nav>
+          <Nav.Link href="#signup">SignUp</Nav.Link>
+          <Nav.Link eventKey={2} href="#signin">
+            SignIn
+          </Nav.Link>
+        </Nav>
+      </Navbar>
+    );
+}
+
+export default Header;
