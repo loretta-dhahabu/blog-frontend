055222408dcd16b701a6f5a1288f9e93d832cd40
loretta dhahabu <loreydjefwa@gmail.com>
2022-09-23 16:24:30 +0300

    edit header

--- blog/src/components/Header.js
+++ blog/src/components/Header.js
@@ -1,27 +1,28 @@
-import React from 'react'
+import React from "react";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
-
-function Header () {
-    return (
+function Header() {
+  return (
+    <Container>
       <Navbar bg="dark" variant="dark">
         <Container>
           <Navbar.Brand href="#home">Blog</Navbar.Brand>
           <Nav className="me-auto">
             <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#about">About Us</Nav.Link>
-            <Nav.Link href="#contact">Contact Us</Nav.Link>
+            <Nav.Link href="#features">About Us</Nav.Link>
+            <Nav.Link href="#pricing">Contact Us</Nav.Link>
+          </Nav>
+          <Nav>
+            <Nav.Link href="#deets">Signup</Nav.Link>
+            <Nav.Link eventKey={2} href="#memes">
+              Signin
+            </Nav.Link>
           </Nav>
         </Container>
-        <Nav>
-          <Nav.Link href="#signup">SignUp</Nav.Link>
-          <Nav.Link eventKey={2} href="#signin">
-            SignIn
-          </Nav.Link>
-        </Nav>
       </Navbar>
-    );
+    </Container>
+  );
 }
 
 export default Header;
