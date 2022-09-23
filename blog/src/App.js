162950013218cb6002537202ae127b279986e038
loretta dhahabu <loreydjefwa@gmail.com>
2022-09-23 16:29:55 +0300

    import footer

--- blog/src/App.js
+++ blog/src/App.js
@@ -1,13 +1,16 @@
 import './App.css';
+import React from 'react';
 import "bootstrap/dist/css/bootstrap.min.css";
 import Header from './components/Header';
 import Home from './components/Home';
+import Footer from './components/Footer'
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <Home/>
+      <Home />
+      <Footer/>
     </div>
   );
 }
