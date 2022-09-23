192b4ab81340cb161070fe196ea2bc5dfa579a0c
loretta dhahabu <loreydjefwa@gmail.com>
2022-09-23 16:23:39 +0300

    modify home page

--- blog/src/components/Home.js
+++ blog/src/components/Home.js
@@ -2,33 +2,51 @@ import { useEffect, useState } from "react";
 import Container from "react-bootstrap/Container";
 import Articles from "./Articles";
 
-function Home ()
-{
-  const [ articles, setArticles ] = useState( [] );
-  const [ headline, setHeadline ] = useState( [] );
-  useEffect( () =>
-  {
-    fetch( "http://localhost:9292/articles" )
-      .then( ( res ) => res.json() )
-      .then( ( data ) =>
-      {
-        console.log( data );
-        setArticles( data );
-      } );
-  }, [] );
+function Home() {
+  const [articles, setArticles] = useState([]);
+  const [headline, setHeadline] = useState([]);
+  useEffect(() => {
+    fetch("http://localhost:9292/articles")
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setArticles(data);
+        setHeadline(data);
+      });
+  }, []);
 
-  // return the first article from the array
-  const Headline = ({headline}) => {
+  // create a Headline component
+  const Headline = ({ headline }) => {
     return (
-      <div>
-        <h1>{headline.title}</h1>
-        <img src={headline.image_url} alt={headline.title} />
-        <p>{headline.content}</p>
+      <div className="headline-container">
+        <h1 className="blog-title">THE NEW TIMES</h1>
+        <div className="headline">
+          <div className="headline-card">
+            {headline.slice(0, 1).map((article) => (
+              <div className="headline-card-body">
+                <img src={article.image_url} alt={article.title} />
+                <h2>{article.title}</h2>
+                <p>{article.content}</p>
+              </div>
+            ))}
+          </div>
+          <div className="other-headlines">
+            {headline.slice(2, 5).map((article) => (
+              <div className="other-headlines-card">
+                <img src={article.image_url} alt={article.title} />
+                <div>
+                  <h2>{article.title}</h2>
+                  <p>{article.content}</p>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
       </div>
     );
   };
 
-  // pass the headline article as a prop
+  // pass the headline to the Headline component
   return (
     <Container>
       <Headline headline={headline} />
