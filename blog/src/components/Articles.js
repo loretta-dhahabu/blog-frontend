f30f6e5d4305298e68b1a2dfa97e44264eb042af
loretta dhahabu <loreydjefwa@gmail.com>
2022-09-23 16:26:19 +0300

    remove grid cards

--- blog/src/components/Articles.js
+++ blog/src/components/Articles.js
@@ -1,19 +1,13 @@
-import { Button, Row, Card, Col, Container } from "react-bootstrap";
 import Article from "./Article";
 
 // define function Articles
 function Articles({ articles }) {
   return (
-    <Container>
-      {/* get the rest of the articles from the array  */}
-      <Row xs={4} md={3} className="g-8">
-        {articles.slice(0, 15).map((article) => (
-          <Col key={article.id}>
-            <Article article={article} key={article.id} />
-          </Col>
-        ))}
-      </Row>
-    </Container>
+    <div className="articles-container">
+      {articles.map((article) => (
+        <Article key={article.id} article={article} />
+      ))}
+    </div>
   );
 }
 
