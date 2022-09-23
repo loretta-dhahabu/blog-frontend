16623eed7418dc6c898ac48101df53a182281fc1
loretta dhahabu <loreydjefwa@gmail.com>
2022-09-23 11:04:38 +0300

    add article

--- blog/src/components/Articles.js
+++ blog/src/components/Articles.js
@@ -1,9 +1,20 @@
 import React from 'react'
+// import Card from "react-bootstrap/Card";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
+import Article from './Article';
 
-function Articles () {
+
+function Articles ({article}) {
     return (
-      <div>Articles</div>
-    )
+      <Row xs={1} md={2} className="g-4">
+        {Array.from({ length: 4 }).map((_, idx) => (
+          <Col>
+                <Article article={ article } />
+          </Col>
+        ))}
+      </Row>
+    );
 }
 
 export default Articles;
